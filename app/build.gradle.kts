2660fd281f73206250d447ed968bb2567e1349d8
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-26 12:05:14 +0300

    updated: viewmodel intro and setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,4 +49,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel:2.8.6")
+    implementation("androidx.activity:activity-ktx:1.9.2")
 }
